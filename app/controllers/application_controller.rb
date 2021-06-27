c28791b7e944bdf8fca3642aa8c6cfaa003387ca
Tejovanth N <tj@peppercontent.in>
2021-06-27 21:21:46 +0530

    init

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters; end
+end
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,21 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import '@doabit/semantic-ui-sass'
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+$(document).on("turbolinks:load", () => {
+  $('.ui.sidebar').sidebar({
+    context: $('.ui.pushable.segment'),
+    transition: 'overlay'
+  }).sidebar('attach events', '#mobile_item');
+});
